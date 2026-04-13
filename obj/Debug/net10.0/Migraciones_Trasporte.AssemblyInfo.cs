4490db46247ac61b73f9ba0984725fe252ca861e
corredor29 <197847818+corredor29@users.noreply.github.com>
2026-04-12 20:50:21 -0500

    feat: :sparkles: se agrego estructuracion

--- obj/Debug/net10.0/Migraciones_Trasporte.AssemblyInfo.cs
+++ obj/Debug/net10.0/Migraciones_Trasporte.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Migraciones_Trasporte")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70363dc921f4d30a2d7ec5f4769b100d62910edb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4eda95eb6bf6980a4740c4021e3c935e750fd0ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("Migraciones_Trasporte")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Migraciones_Trasporte")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
